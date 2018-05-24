25659b3c2a97fe9dcb17b996be987691f999ddf4
VioletIzHere <zoeisaduck@gmail.com>
2018-05-24 18:34:58 -0400

    added pymongo stuff

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import pymongo
+
+uri = "mongodb://127.0.0.1:27017"
+client = pymongo.MongoClient(uri)
+database = client['blog_app']
+collection = database['users']
+
+collection.remove({})
+
+collection.insert({"name": "bobross", "pass": "bob"})
+
+users = collection.find({})
+for user in users:
+    print(user)
